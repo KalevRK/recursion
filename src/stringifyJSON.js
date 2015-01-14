b7bfce72bdcde8060646e13e710b478747f6c329
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-01-14 15:27:12 -0800

    Added helper function for recursion and outlined each step of the helper function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,36 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var indent = '  ';
+  return stringify("", {"": obj});
+};
+
+var stringify = function(key, container) {
+  // helper function which is called recursively to generate a string from a JSON object
+
+  // initialize variables
+
+  // check to see if value has a toJSON method
+
+  // return value based on type of value
+  
+  // String case
+
+  // Number case
+
+  // Boolean case
+
+  // Null case
+
+  // Object case
+
+    // Check whether value is array
+      // if array then stringify every element
+
+      // join elements together, separate with commas, and wrap in square brackets
+
+    // otherwise iterate through all the keys of the object value
+
+      // join elements together, separate with commas, and wrap in curly braces
+      
 };
