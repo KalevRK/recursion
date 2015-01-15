75576d178b67748006206bde824c70831ae1a5d7
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-01-14 17:38:02 -0800

    Implemented the getElementsByClassName function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,20 +8,31 @@ var getElementsByClassName = function(className){
   // your code here
   // Call a helper function on the root of the document
   return getElementsHelper(document.body, className, []);
+  
 };
 
 // Helper function for recursion
 // element = current element being inspected
 // className = class name to match on
 // nodeList = array of all nodes that have matched so far
-var getElementsHelper = function(element, className, nodeList) {
-    
+var getElementsHelper = function(node, className, nodeList) {
+
     // check if current element has matching class
+    if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains(className)) {
+    	nodeList.push(node);
+    }
 
     // check for base case - element with no children element
     // if element has children then pop on the result of
     // calling getElementsHelper on each child element onto the nodeList array
+    console.log(node.childNodes);
+    if (node.childNodes.length > 0) {
+      for (var i=0; i < node.childNodes.length; i++) {
+        getElementsHelper(node.childNodes[i], className, nodeList);
+      }
+    }
 
     // Return the array of node names
+    return nodeList;
 
 }
