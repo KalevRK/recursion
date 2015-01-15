987e6baeeb49f69173f1471e52a635f424b21597
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-01-14 17:02:45 -0800

    Added helper function for recursion and outlined code for implementation.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  // Call a helper function on the root of the document
+  return getElementsHelper(document.body, className, []);
 };
+
+// Helper function for recursion
+// element = current element being inspected
+// className = class name to match on
+// nodeList = array of all nodes that have matched so far
+var getElementsHelper = function(element, className, nodeList) {
+    
+    // check if current element has matching class
+
+    // check for base case - element with no children element
+    // if element has children then pop on the result of
+    // calling getElementsHelper on each child element onto the nodeList array
+
+    // Return the array of node names
+
+}
